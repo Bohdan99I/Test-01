387991d1e760eb6b228edec2b94a7448bc5edeeb
Bohdan99I <bohdan.voiyov@gmail.com>
2023-11-10 20:53:04 +0200

    0.4

--- controllers/users.js
+++ controllers/users.js
@@ -4,3 +4,5 @@ const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
+
+const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
